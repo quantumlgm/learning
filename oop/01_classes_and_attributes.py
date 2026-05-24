f1062a2a7d666c65b18b05ea976e4790ae506c60
Руслан <v209861@gmail.com>
2026-05-24 23:01:44 +0300

    refactor(01_classes_and_attributes): rename class method names

--- oop/01_classes_and_attributes.py
+++ oop/01_classes_and_attributes.py
@@ -26,23 +26,23 @@ class EnviromentConfig:
         self.readonly_keys = readonly_keys
         print('Succesfully create')
 
-    def get_setting(self, key):
+    def get_config(self, key):
         print(getattr(self, key, "NOT_FOUND"))
     
-    def set_setting(self, new_dict):
+    def set_config(self, new_dict):
         for key, value in new_dict.items():
             setattr(self, key, value)
             print(f'Successfully add: {key}: {value}')
 
-    def update_setting(self, key, value):
+    def update_config(self, key, value) -> None:
         if self.__dict__[key]:   
             if key not in self.readonly_keys:         
                 setattr(self, key, value)
                 print('Successfully update')
             else:
-                print('Item in the private list')
+                print('Item in the private list')           
 
-    def del_setting(self, key):
+    def del_config(self, key):
         if self.__dict__[key]:
             if key not in self.readonly_keys:         
                 delattr(self, key)
@@ -52,11 +52,11 @@ class EnviromentConfig:
             
 if __name__ == '__main__':
     Enviroment = EnviromentConfig({'set1': 1, 'set2': 2}, ('set1')) # Succesfully create
-    Enviroment.get_setting("set1") # 1
-    Enviroment.set_setting({'set3': 3, 'set4': '4'}) # Successfully add: set3: 3 and Successfully add: set4: 4
-    Enviroment.update_setting('set3', 30) # Successfully update
-    Enviroment.update_setting('set1', 10) # Item in the private list
-    Enviroment.del_setting('set3') # Successfully delete
-    Enviroment.del_setting('set1') # Item in the private list
+    Enviroment.get_config("set1") # 1
+    Enviroment.set_config({'set3': 3, 'set4': '4'}) # Successfully add: set3: 3 and Successfully add: set4: 4
+    Enviroment.updateconfig({'set3', 30}) # Successfully update
+    Enviroment.update_config('set1', 10) # Item in the private list
+    Enviroment.del_config('set3') # Successfully delete
+    Enviroment.del_config('set1') # Item in the private list
 
 
