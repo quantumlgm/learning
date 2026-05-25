12d68eba4aeeb767f275fffde5d000784e79bed5
Руслан <v209861@gmail.com>
2026-05-25 22:52:53 +0300

    feat(04_singleton_and_new): implement class with methods and tests

--- oop/04_singleton_and_new.py
+++ oop/04_singleton_and_new.py
@@ -14,3 +14,30 @@
 
 # 3. Interface:
 #    - `log(self, message: str) -> None`: Prints the message to the console in the following format: `[{log_level}] {message}`.
+
+
+class GlobalLogger:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+        
+    def __init__(self, log_level: str) -> None:
+        if hasattr(self, 'log_level'):
+            return
+        self.log_level = log_level
+
+    def log(self, message: str) -> str:
+        return f"[{self.log_level}] {message}"
+    
+
+if __name__ == "__main__":
+    print(GlobalLogger.__dict__) # '_GlobalLogger__instance': None
+    Logger = GlobalLogger('INFO') 
+    print(Logger.__dict__)# {'log_level': 'INFO'}
+    print(GlobalLogger.__dict__) # _GlobalLogger__instance': <__main__.GlobalLogger object at 0x0000022995C56CF0>
+    Logger = GlobalLogger('DEBUG') 
+    print(Logger.__dict__) # {'log_level': 'INFO'}
+    print(GlobalLogger.__dict__) # _GlobalLogger__instance': <__main__.GlobalLogger object at 0x0000022995C56CF0>
