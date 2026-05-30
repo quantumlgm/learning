e7627b83e3484fd29791c05b72b77a50807c5633
Руслан <v209861@gmail.com>
2026-05-30 19:30:37 +0300

    feat(13_magic_methods_basics): implement Track class with magic methods (__repr__, __str__, __len__, __abs__)

--- oop/13_magic_methods_basics.py.py
+++ oop/13_magic_methods_basics.py.py
@@ -13,3 +13,31 @@
 #    - Implement `__len__`: Returns the track's duration as an integer.
 #    - Implement `__abs__`: Returns the absolute value of the db_level (removing the negative sign).
 
+
+class Track:
+    def __init__(self, title: str, artist: str, duration: int, db_level: int) -> None:
+        self._title = title
+        self._artist = artist
+        self._duration = duration
+        self._db_level = db_level
+
+    def __repr__(self):
+        return f'Track({self._title}, {self._artist}, {self._duration}, {self._db_level})'
+    
+    def __str__(self):
+        return f'{self._artist} - {self._title} ({self._duration} sec.)'        
+    
+    def __len__(self):
+        return self._duration
+    
+    def __abs__(self):
+        return abs(self._db_level)
+    
+
+if __name__ == '__main__':
+    track = Track("Blinding Lights", "The Weeknd", 200, -6)
+
+    print(repr(track)) # Track('Blinding Lights', 'The Weeknd', 200, -6)
+    print(track)       # The Weeknd - Blinding Lights (200 sec.)
+    print(len(track))  # 200
+    print(abs(track))  # 6
