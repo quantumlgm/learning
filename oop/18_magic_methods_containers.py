b0ce5f9099a2e69609613eac5faeb34b53592a0a
Руслан <v209861@gmail.com>
2026-06-01 20:53:56 +0300

    feat(18_magic_methods_containers): implement StudentJournal container methods (__getitem__, __setitem__, __delitem__)

--- oop/18_magic_methods_containers.py
+++ oop/18_magic_methods_containers.py
@@ -6,3 +6,41 @@
 # 2. Implement retrieval by subject name.
 # 3. Implement assignment/update by subject name.
 # 4. Implement deletion by subject name.
+
+
+class StudentJournal:
+    def __init__(self, name: str, marks: dict) -> None:
+        self.name = name
+        if not isinstance(marks, dict):
+            raise TypeError('The data type is incorrect')
+        self.marks = marks
+
+    def __getitem__(self, key: str) -> int:
+        if key not in self.marks:
+            raise ValueError('The item is not in the list')
+        return self.marks[key]
+    
+    def __setitem__(self, key: str, value: int) -> None:
+        self.marks[key] = value        
+
+    def __delitem__(self, key: str) -> None:
+        if key not in self.marks:
+            raise ValueError('The item is not in the list')
+        del self.marks[key]
+    
+
+if __name__ == '__main__':
+    Student = StudentJournal('Ruslan', {'Math': 5, 'History': 4, 'CS': 5})
+    print(Student['Math']) # 5
+    print(Student.marks) # {'Math': 5, 'History': 4, 'CS': 5}
+
+    Student['Math'] = 4
+    print(Student['Math']) # 4
+    print(Student.marks) # {'Math': 4, 'History': 4, 'CS': 5}
+
+    del Student['History']
+    print(Student['History']) # ValueError: The item is not in the list 
+    print(Student.marks) # {'Math': 4, 'CS': 5}
+
+
+
