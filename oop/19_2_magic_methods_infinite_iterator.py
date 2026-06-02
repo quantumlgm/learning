1e613aac45d7115e610c5eab7cec638b05d31ebf
Руслан <v209861@gmail.com>
2026-06-02 16:38:39 +0300

    feat(19_2_magic_methods_infinite_iterator): implement infinite playlist iterator

--- oop/19_2_magic_methods_infinite_iterator.py
+++ oop/19_2_magic_methods_infinite_iterator.py
@@ -9,3 +9,26 @@
 #    advance the internal pointer to the next track.
 # 4. The iterator must be infinite and never raise `StopIteration`. 
 #    When the end of the playlist is reached, it must seamlessly loop back to the first track.
+
+
+class TrackPlaylist:
+    def __init__(self, playlist: list) -> None:
+        if not isinstance(playlist, list):
+            raise ValueError("The value must be a list")
+        self.playlist = playlist
+        self.index = 0
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):        
+        if self.index == len(self.playlist):
+            self.index = 0
+        track = self.index
+        self.index += 1
+        return self.playlist[track]
+
+if __name__ == '__main__':
+    Track = TrackPlaylist(['Song A', 'Song B', 'Song C'])
+    for t in Track:
+        print(t)
