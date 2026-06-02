c0a11eab30bd633177a33a2ab5edb0ef01fa5215
Руслан <v209861@gmail.com>
2026-06-02 19:35:08 +0300

    feat(21_builtins_inheritance_issubclass): implement LoggingList and StringLoggingList with method overriding

--- oop/21_builtins_inheritance_issubclass.py
+++ oop/21_builtins_inheritance_issubclass.py
@@ -14,3 +14,28 @@
 
 # 3. Validation:
 #    - In the execution block, check class hierarchies using `issubclass()`.
+
+
+from typing import Any
+
+
+class LoggingList(list):
+    def append(self, item: Any) -> None:
+        print(f'Item was added: {item}')
+        list.append(self, item)
+
+
+class StringLoggingList(LoggingList):
+    def append(self, item: str) -> None:
+        if not isinstance(item, str):
+            raise ValueError("Value must be string")
+        LoggingList.append(self, item)
+        
+    
+if __name__ == '__main__':
+    logging_list = LoggingList()
+    logging_list.append('Quantumlgm') # Item was added: Quantumlgm
+    
+    string_logging_list = StringLoggingList()
+    string_logging_list.append('Quantumlgm String') # Item was added: Quantumlgm String
+#   string_logging_list.append(100) -> ValueError: Value must be string
