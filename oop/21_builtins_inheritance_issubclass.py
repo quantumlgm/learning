0a527ad7b676c92ccbdf35b7587b042c9326877a
Руслан <v209861@gmail.com>
2026-06-02 19:49:00 +0300

    feat(21_builtins_inheritance_issubclass): add LoggingList inheritance with issubclass examples

--- oop/21_builtins_inheritance_issubclass.py
+++ oop/21_builtins_inheritance_issubclass.py
@@ -1,15 +1,15 @@
-# Design two classes, `LoggingList` and `StringLoggingList`, by extending 
+# Design two classes, `LoggingList` and `StringLoggingList`, by extending
 # Python's built-in `list` type, and validate their hierarchy.
 
 # Requirements:
 # 1. Subclass `LoggingList`:
 #    - Inherits directly from the built-in `list` class.
-#    - Overrides the standard element-appending method to print 
+#    - Overrides the standard element-appending method to print
 #      f"Add item: {item}" to the console right before appending the item.
 
 # 2. Subclass `StringLoggingList`:
 #    - Inherits from `LoggingList`.
-#    - Overrides the same element-appending method to strictly allow only 
+#    - Overrides the same element-appending method to strictly allow only
 #      strings (raise TypeError otherwise).
 
 # 3. Validation:
@@ -21,7 +21,7 @@ from typing import Any
 
 class LoggingList(list):
     def append(self, item: Any) -> None:
-        print(f'Item was added: {item}')
+        print(f"Item was added: {item}")
         list.append(self, item)
 
 
@@ -30,12 +30,14 @@ class StringLoggingList(LoggingList):
         if not isinstance(item, str):
             raise ValueError("Value must be string")
         LoggingList.append(self, item)
-        
-    
-if __name__ == '__main__':
-    logging_list = LoggingList()
-    logging_list.append('Quantumlgm') # Item was added: Quantumlgm
-    
-    string_logging_list = StringLoggingList()
-    string_logging_list.append('Quantumlgm String') # Item was added: Quantumlgm String
-#   string_logging_list.append(100) -> ValueError: Value must be string
+
+
+if __name__ == "__main__":
+    print(issubclass(LoggingList, list))
+    print(issubclass(LoggingList, object))
+    print(issubclass(StringLoggingList, LoggingList))
+
+    logging_list = StringLoggingList()
+    logging_list.append("Quantumlgm")  # Item was added: Quantumlgm
+    print(logging_list)  # ['Quantumlgm']
+    logging_list.append(111)  # ValueError: Value must be string
