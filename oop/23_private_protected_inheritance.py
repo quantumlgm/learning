8712029f3a0806c7067707edc25ab2733e02a8a2
Руслан <v209861@gmail.com>
2026-06-03 20:25:44 +0300

    feat(23_private_protected_inheritance): demonstrate protected and private attribute inheritance

--- oop/23_private_protected_inheritance.py
+++ oop/23_private_protected_inheritance.py
@@ -17,3 +17,39 @@
 #      Return "Access granted" or "Access denied" accordingly.
 #    - Implement a simulation method that attempts to directly break into the 
 #      parent's hidden master key to test encapsulation limits.
+
+
+class BaseVault:
+    def __init__(self, name: str, key: str) -> None:
+        self._name = name
+        self.__key = key
+    
+    def _check_key(self, key: str) -> None:
+        return key == self.__key
+    
+
+class SubVault(BaseVault):
+    def __init__(self, name: str, key: str) -> None:
+        super().__init__(name, key)
+    
+    def get_name(self) -> None:
+        return self._name
+    
+    def open_safe(self, key: str) -> None:
+        if super()._check_key(key):
+            return 'Acces granted'
+        return 'Access denied'
+    
+    def hack(self):
+        return self.__key
+
+
+if __name__ == '__main__':
+    base = BaseVault('base', 'base_key')
+    sub = SubVault('sub', 'sub_key')
+    print(sub.__dict__) # {'_name': 'sub', '_BaseVault__key': 'sub_key'}
+
+    print(sub.get_name()) # sub
+    print(sub.open_safe('another_key')) # Access denied
+    print(sub.open_safe('sub_key')) # Access granted
+    # print(sub.hack()) # AttributeError: 'SubVault' object has no attribute '_SubVault__key'. Did you mean: '_BaseVault__key'?
