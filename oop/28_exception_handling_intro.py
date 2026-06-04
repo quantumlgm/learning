4c62e58f8257971b960cbdc1dcb321e8c05ed4b8
Руслан <v209861@gmail.com>
2026-06-04 20:44:00 +0300

    feat(28_exception_handling_intro): implement SafeDivider with exception handling

--- oop/28_exception_handling_intro.py
+++ oop/28_exception_handling_intro.py
@@ -11,3 +11,24 @@
 #    - Catch any other generic `Exception` as a fallback.
 #    - Return appropriate error messages as specified in the instructions 
 #      rather than letting the application crash.
+
+
+class SafeDivider:
+    def divide(self, val1: int | float | str, val2: int | float | str) -> int | float | str:
+        try:
+            val1, val2 = float(val1), float(val2)
+            return val1 / val2
+        except ValueError:
+            return "Error: Inputs must be numeric values"
+        except ZeroDivisionError:
+            return "Error: Division by zero is forbidden"
+        except Exception as e:
+            return f"Unexpected error: {e}"
+
+
+if __name__ == '__main__':
+    devider = SafeDivider()
+    print(devider.divide(10, 5)) # 2.0
+    print(devider.divide(10, "2")) # 5.0
+    print(devider.divide(10, 0)) # Error: Division by zero is forbidden
+    print(devider.divide(10, 'a')) # Error: Inputs must be numeric values
