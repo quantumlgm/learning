713a88a0fb0f012fbe391a468de06eab80f2fcb7
Руслан <v209861@gmail.com>
2026-06-14 19:04:47 +0300

    style(02_function_contracts_and_callable.py): apply ruff formatting

--- technologies/Pyhton/typing/02_function_contracts_and_callable.py
+++ technologies/Pyhton/typing/02_function_contracts_and_callable.py
@@ -2,37 +2,45 @@ from typing import Callable
 
 type number = int | float
 
+
 class RegisterCustomer:
     def __init__(self, name: str, loyalty: int, balance: number) -> None:
         self.name = name
         self.loyalty = loyalty
         self.balance = balance
 
+
 def loyalty_program(customer: RegisterCustomer, cost: number) -> number:
     discount = cost * customer.loyalty / 100
     return discount
+
 
 def black_friday_sale(customer: RegisterCustomer, cost: number) -> number:
     if cost > 5000:
         return 1000
     return 0.0
 
-def payment_processing(cost: number, customer: RegisterCustomer, promotion: Callable[[RegisterCustomer, number], number]) -> number:
+
+def payment_processing(
+    cost: number,
+    customer: RegisterCustomer,
+    promotion: Callable[[RegisterCustomer, number], number],
+) -> number:
     discount_amount: number = promotion(customer, cost)
     if discount_amount >= cost:
         return 0.0
     total = cost - discount_amount
-    return total    
+    return total
+
 
 if __name__ == "__main__":
-    ruslan = RegisterCustomer('Ruslan', 15, 5000)
-    print(ruslan.__dict__) # {'name': 'Ruslan', 'loyalty': 15, 'balance': 0}
+    ruslan = RegisterCustomer("Ruslan", 15, 5000)
+    print(ruslan.__dict__)  # {'name': 'Ruslan', 'loyalty': 15, 'balance': 0}
 
     shoes = 6000
 
     ruslan_payment = payment_processing(shoes, ruslan, loyalty_program)
-    print(ruslan_payment) # 5100.0
+    print(ruslan_payment)  # 5100.0
 
     ruslan_payment = payment_processing(shoes, ruslan, black_friday_sale)
-    print(ruslan_payment) # 5000
-
+    print(ruslan_payment)  # 5000
