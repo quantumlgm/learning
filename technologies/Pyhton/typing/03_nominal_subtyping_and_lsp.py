79cf0a1e87b8f638e69e5e390a5b214bd9e05931
Руслан <v209861@gmail.com>
2026-06-15 17:14:12 +0300

    style(03_nominal_subtyping_and_lsp.py): apply ruff formatting

--- technologies/Pyhton/typing/03_nominal_subtyping_and_lsp.py
+++ technologies/Pyhton/typing/03_nominal_subtyping_and_lsp.py
@@ -1,7 +1,5 @@
-
-
-
 type number = int | float
+
 
 class BaseOrder:
     def __init__(self, procurement_item: str, cost: number) -> None:
@@ -10,40 +8,46 @@ class BaseOrder:
 
     def calculate_total_cost(self):
         return self.cost
-    
+
 
 class UrgentOrder(BaseOrder):
     def __init__(self, procurement_item: str, cost: number) -> None:
         cost += 1500
-        super().__init__(procurement_item, cost)    
+        super().__init__(procurement_item, cost)
+
 
 class WholesaleOrder(BaseOrder):
     def __init__(self, procurement_item: str, cost: number, quantity: int) -> None:
         total = cost * quantity
         if quantity > 10:
             total *= 0.9
-        super().__init__(procurement_item, total)    
-        self.quantity = quantity    
+        super().__init__(procurement_item, total)
+        self.quantity = quantity
+
 
 def application_module(order: BaseOrder) -> number:
     return order.calculate_total_cost()
 
+
 def process_corporate_purchase(order: BaseOrder, budget: number) -> number | str:
     if order.cost < budget:
         return budget - order.cost
-    return 'Not enough money'
+    return "Not enough money"
+
 
 if __name__ == "__main__":
-    urgent_order = UrgentOrder('Black board', 500)
+    urgent_order = UrgentOrder("Black board", 500)
     print(urgent_order.__dict__)  # {'procurement_item': 'Black board', 'cost': 2000}
 
-    wholesale_order = WholesaleOrder('Notebook', 800, 15)
-    print(wholesale_order.__dict__)  # {'procurement_item': 'Notebook', 'cost': 1350.0, 'quantity': 15}
+    wholesale_order = WholesaleOrder("Notebook", 800, 15)
+    print(
+        wholesale_order.__dict__
+    )  # {'procurement_item': 'Notebook', 'cost': 1350.0, 'quantity': 15}
 
     budget = 3000
 
-    print(process_corporate_purchase(urgent_order, budget)) # 1000
-    print(process_corporate_purchase(wholesale_order, budget)) # Not enough money
+    print(process_corporate_purchase(urgent_order, budget))  # 1000
+    print(process_corporate_purchase(wholesale_order, budget))  # Not enough money
 
     """
     class ErrorOrder: <-- Pyright will highlight this section in red
@@ -52,5 +56,3 @@ if __name__ == "__main__":
     print(process_corporate_purchase(ErrorOrder, budget)) <-- AttributeError: type object 'ErrorOrder' has no attribute 'cost'
 
     """
-
-
