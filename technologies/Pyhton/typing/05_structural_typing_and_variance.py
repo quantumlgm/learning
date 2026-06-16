ad42b692f9311072b0b6f34780da0de13784bf59
Руслан <v209861@gmail.com>
2026-06-16 19:55:29 +0300

    style(05_structural_typing_and_variance.py): apply ruff formatting

---
+++ technologies/Pyhton/typing/05_structural_typing_and_variance.py
@@ -0,0 +1,55 @@
+from typing import Protocol, Iterable
+from dataclasses import dataclass
+
+
+class Publication(Protocol):
+    views: int
+
+    def play(self) -> None: ...
+
+
+@dataclass
+class Article:
+    views: int
+
+    def play(self) -> None:
+        print("Read the article")
+
+
+@dataclass
+class Video:
+    views: int
+
+    def play(self) -> None:
+        print("Watch the wideo")
+
+
+def analytics(publications: Iterable[Publication]) -> int:
+    return sum(p.views for p in publications)
+
+
+def moderation(publications: Iterable[Publication]):
+    collection: dict[str, list[Publication]] = {"Article": [], "Video": []}
+
+    for p in publications:
+        if isinstance(p, Article):
+            collection["Article"].append(p)
+        elif isinstance(p, Video):
+            collection["Video"].append(p)
+
+    return collection
+
+
+if __name__ == "__main__":
+    article = Article(100)
+    article.play()  # Read the article
+
+    video = Video(200)
+    video.play()  # Wathc the wideo
+
+    print(analytics([article, video]))  # 300
+
+    zoo_video = Video(1000)
+    sea_article = Article(300)
+    print(moderation([article, sea_article, video, zoo_video]))
+    # {'Article': [Article(views=100), Article(views=300)], 'Video': [Video(views=200), Video(views=1000)]}
