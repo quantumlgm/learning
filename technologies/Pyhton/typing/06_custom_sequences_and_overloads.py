2a226e641ecad542c3f19e71b87888a4a4e26cbd
Руслан <v209861@gmail.com>
2026-06-17 17:39:30 +0300

    style(06_custom_sequences_and_overloads.py): apply ruff formatting

--- technologies/Pyhton/typing/06_custom_sequences_and_overloads.py
+++ technologies/Pyhton/typing/06_custom_sequences_and_overloads.py
@@ -13,31 +13,31 @@ class RequestLog:
 
 
 class RequestHistory(Sequence[RequestLog]):
-
-    def __init__(self, data: list[RequestLog]):        
+    def __init__(self, data: list[RequestLog]):
         self._storage: dict[int, RequestLog] = {}
         for key, value in enumerate(data):
             self._storage[key] = value
 
     def __len__(self) -> int:
         return len(self._storage)
-    
+
     @overload
     def __getitem__(self, key: int) -> RequestLog: ...
 
     @overload
     def __getitem__(self, key: slice) -> Sequence[RequestLog]: ...
-    
-    def __getitem__(self, key: int | slice) -> RequestLog | Sequence[RequestLog]:        
+
+    def __getitem__(self, key: int | slice) -> RequestLog | Sequence[RequestLog]:
         all_logs = list(self._storage.values())
-        if isinstance(key, slice):            
-            sliced_logs = all_logs[key]        
-            return RequestHistory(sliced_logs)        
+        if isinstance(key, slice):
+            sliced_logs = all_logs[key]
+            return RequestHistory(sliced_logs)
         return all_logs[key]
-    
+
     def __repr__(self) -> str:
-        logs_list = list(self._storage.values())        
+        logs_list = list(self._storage.values())
         return f"RequestHistory({logs_list})"
+
 
 if __name__ == "__main__":
     error_log = RequestLog("error", "https://www.a", 10)
@@ -45,6 +45,8 @@ if __name__ == "__main__":
     load_log = RequestLog("load", "https://www.c", 30)
 
     history = RequestHistory([error_log, success_log, load_log])
-    print(len(history)) # 3
-    print(history[1]) # RequestLog(content='success', url='https://www.b', data=20)
-    print(history[0:2]) # RequestHistory([RequestLog(content='error', url='https://www.a', data=10), RequestLog(content='success', url='https://www.b', data=20)])
+    print(len(history))  # 3
+    print(history[1])  # RequestLog(content='success', url='https://www.b', data=20)
+    print(
+        history[0:2]
+    )  # RequestHistory([RequestLog(content='error', url='https://www.a', data=10), RequestLog(content='success', url='https://www.b', data=20)])
