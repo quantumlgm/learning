b930921c16fe256162662dddecb8a0a7752e795b
Руслан <v209861@gmail.com>
2026-06-17 19:37:16 +0300

    style(07_lsp_notification_system.py): apply ruff formatting

--- technologies/Pyhton/typing/07_lsp_notification_system.py
+++ technologies/Pyhton/typing/07_lsp_notification_system.py
@@ -1,8 +1,8 @@
 import random
 from collections.abc import Sequence
 
-class Notification:
 
+class Notification:
     def __init__(self) -> None:
         self.messages: list[str] = []
 
@@ -11,25 +11,29 @@ class Notification:
         if result:
             self.messages.append(message)
         return result
-        
+
+
 class EmailNotification(Notification):
     def send(self, message: str | Sequence[str]) -> bool:
         result = random.choice([True, False])
         if result:
             if isinstance(message, str):
                 self.messages.append(message)
-            else:                
+            else:
                 self.messages.extend(message)
         return result
-        
+
+
 class SMSNotification(Notification):
     def send(self, message: str, phone: int = 900) -> bool:
         result = random.choice([True, False])
-        if result:            
+        if result:
             self.messages.append(f"[{phone}]: {message}")
         return result
 
+
 if __name__ == "__main__":
+
     def delivery(notifier: Notification) -> None:
         success = notifier.send("Hello!")
         print(f"Status of answer: {success}, log: {notifier.messages}")
@@ -38,6 +42,6 @@ if __name__ == "__main__":
     email_notif = EmailNotification()
     sms_notif = SMSNotification()
 
-    delivery(base_notif) # Status of answer: False, log: []
-    delivery(email_notif) # Status of answer: True, log: ['Hello!']
-    delivery(sms_notif) # Status of answer: False, log: []
+    delivery(base_notif)  # Status of answer: False, log: []
+    delivery(email_notif)  # Status of answer: True, log: ['Hello!']
+    delivery(sms_notif)  # Status of answer: False, log: []
