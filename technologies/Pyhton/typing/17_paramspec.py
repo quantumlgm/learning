f28780ffcfd14fb907af36156a4b9168652a7aa3
Руслан <v209861@gmail.com>
2026-06-21 18:52:26 +0300

    feat(18_never.py): add initial file

--- technologies/Pyhton/typing/17_paramspec.py
+++ technologies/Pyhton/typing/17_paramspec.py
@@ -8,7 +8,7 @@ Detailed Description:
 This module demonstrates advanced typing for decorators and closures:
 - Uses 'ParamSpec' (P) to capture full argument signatures, preventing type erasure.
 - Leverages 'P.args' and 'P.kwargs' to pass parameters safely through wrappers.
-- Employs 'Concatenate' to explicitly model signature mutation, removing the first 
+- Employs 'Concatenate' to explicitly model signature mutation, removing the first
   'Database' positional argument from the decorated function's external interface.
 """
 
@@ -22,26 +22,33 @@ P = ParamSpec("P")
 class Database:
     pass
 
+
 def simple_logger(func: Callable[P, T]):
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         print("The function starts")
         return func(*args, **kwargs)
+
     return wrapper
+
 
 def inject_database(func: Callable[Concatenate[Database, P], T]):
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         db_obj = Database()
         return func(db_obj, *args, **kwargs)
+
     return wrapper
+
 
 @simple_logger
 def send_message(username: str, text: str) -> tuple[str, str]:
     return username, text
 
+
 @inject_database
 def get_user_data(db: Database, user_id: int, token: str) -> tuple[Database, int, str]:
     return db, user_id, token
 
+
 if __name__ == "__main__":
-    send_message('Hello', "world!") # The function starts
+    send_message("Hello", "world!")  # The function starts
     get_user_data(user_id=1, token="@token111")
