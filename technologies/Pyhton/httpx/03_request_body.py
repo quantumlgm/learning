20e4ef44ac5a5d8e94ea526cf0a79418777518c0
Руслан <v209861@gmail.com>
2026-06-28 23:06:41 +0300

    style(03_request_body.py): apply ruff formatting

--- technologies/Pyhton/httpx/03_request_body.py
+++ technologies/Pyhton/httpx/03_request_body.py
@@ -1,22 +1,21 @@
 from rich import print
 import asyncio
-import httpx 
+import httpx
 
 payload = {
     "gateway_id": "GW-007-TXT",
     "is_active": True,
     "temperatures": [22.5, 24.0, 21.8],
-    "version": 2
+    "version": 2,
 }
+
 
 async def main():
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://httpbin.org/post",
-            json=payload            
-        )
+        response = await client.post("https://httpbin.org/post", json=payload)
     data = response.json()
-    print(f"Status: {response.status_code}. ID: {data["json"]["gateway_id"]}")
+    print(f"Status: {response.status_code}. ID: {data['json']['gateway_id']}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
