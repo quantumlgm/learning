a0b9af5bb9cad7117800b4a868eca51e4648dd74
Руслан <v209861@gmail.com>
2026-06-29 19:10:38 +0300

    feat(01_minimal_app.py): implement exercise

---
+++ technologies/Python/fastapi/01_minimal_app.py
@@ -0,0 +1,19 @@
+"""
+Lesson 01: Creating a Minimal FastAPI Application
+
+This script demonstrates the most basic configuration of a FastAPI server.
+It includes a single REST API endpoint and demonstrates how to programmatically
+launch the Uvicorn ASGI server with hot-reloading enabled.
+"""
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/", summary="General", tags=["General roots"])
+def root():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    uvicorn.run("01_minimal_app:app", reload=True)
