a39f0b73c443a2d16fa2531ff001c631524279eb
Руслан <v209861@gmail.com>
2026-06-29 20:46:33 +0300

    style(03_fastapi_pydentic.py): apply ruff formatting

--- technologies/Python/fastapi/03_fastapi_pydentic.py
+++ technologies/Python/fastapi/03_fastapi_pydentic.py
@@ -1,9 +1,9 @@
 """
 Lesson 03: Data Validation using Pydantic Models and ConfigDict
 
-This script demonstrates data schema definition and validation techniques in 
-FastAPI using Pydantic. It showcases class inheritance to extend data structures, 
-field constraint enforcement using the Field function, and explicit validation 
+This script demonstrates data schema definition and validation techniques in
+FastAPI using Pydantic. It showcases class inheritance to extend data structures,
+field constraint enforcement using the Field function, and explicit validation
 behavior modifications by leveraging ConfigDict to strictly forbid extra fields.
 """
 
@@ -13,26 +13,27 @@ import uvicorn
 
 app = FastAPI()
 
-data = {
-    "email": 'quantumlgm@gmail.com',
-    "name": "Quantumlgm",
-    "age": 16
-}
+data = {"email": "quantumlgm@gmail.com", "name": "Quantumlgm", "age": 16}
+
 
 class UserData(BaseModel):
     email: str
     name: str
     model_config = ConfigDict(extra="forbid")
-    
+
+
 class UserAge(UserData):
     age: int = Field(ge=0, le=130)
 
+
 users = []
+
 
 @app.post("/users")
 def add_users(user: UserData):
     users.append(user)
     return {"status": "ok"}
+
 
 @app.get("/users")
 def get_users():
