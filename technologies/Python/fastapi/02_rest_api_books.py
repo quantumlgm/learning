16ea2ca10f898cf61defd3657a54742ab1acd71d
Руслан <v209861@gmail.com>
2026-06-29 20:14:14 +0300

    style(02_rest_api_books.py): apply ruff formatting

--- technologies/Python/fastapi/02_rest_api_books.py
+++ technologies/Python/fastapi/02_rest_api_books.py
@@ -2,9 +2,9 @@
 Lesson 02: Building a Basic REST API for Book Management
 
 This script demonstrates the core concepts of handling different HTTP methods
-(GET and POST) using FastAPI. It implements a simple in-memory data store for 
-books, features parameterized path routing for retrieving specific records with 
-automatic type validation, utilizes Pydantic models for request body parsing, 
+(GET and POST) using FastAPI. It implements a simple in-memory data store for
+books, features parameterized path routing for retrieving specific records with
+automatic type validation, utilizes Pydantic models for request body parsing,
 and showcases structured error handling via HTTPException.
 """
 
@@ -20,29 +20,36 @@ books = [
     {"id": 2, "title": "PostgreSQL - Be better", "author": "Quantumlgm"},
 ]
 
-@app.get("/books", tags=["Books"], summary='Get all the books')
+
+@app.get("/books", tags=["Books"], summary="Get all the books")
 def read_books():
     return books
 
-@app.get("/books/{id}", tags=["Books"], summary='Get one of the all books')
+
+@app.get("/books/{id}", tags=["Books"], summary="Get one of the all books")
 def get_book(id: int):
     for b in books:
         if b["id"] == id:
-            return b 
+            return b
     raise HTTPException(status_code=404, detail="The book wasn't found.")
+
 
 class NewBook(BaseModel):
     title: str
     author: str
 
-@app.post('/books', tags=["Books"])
+
+@app.post("/books", tags=["Books"])
 def create_book(new_book: NewBook):
-    books.append({
-        "id": len(books) + 1,
-        "title": new_book.title,
-        "author": new_book.author,
-    })
+    books.append(
+        {
+            "id": len(books) + 1,
+            "title": new_book.title,
+            "author": new_book.author,
+        }
+    )
     return {"success": True}
+
 
 if __name__ == "__main__":
     uvicorn.run("02_rest_api_books:app", reload=True)
