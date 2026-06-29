17d2fa350985e441dabff5204b4c56059f35dd85
Руслан <v209861@gmail.com>
2026-06-29 16:43:51 +0300

    style(05_advanced_gather.py): apply ruff formatting

--- technologies/Pyhton/httpx/05_advanced_gather.py
+++ technologies/Pyhton/httpx/05_advanced_gather.py
@@ -5,23 +5,27 @@ import asyncio
 limits = httpx.Limits(max_connections=2)
 timeout = httpx.Timeout(5)
 
+
 async def fetch(client: httpx.AsyncClient, coin: str):
     try:
         params = {"ids": coin, "vs_currencies": "usd", "localization": False}
 
-        response = await client.get(f"https://api.coingecko.com/api/v3/coins/{coin}", params=params)
+        response = await client.get(
+            f"https://api.coingecko.com/api/v3/coins/{coin}", params=params
+        )
         response.raise_for_status()
-        
-        data = response.json()            
+
+        data = response.json()
         return {
             "Coin": {
                 "id": data["id"],
-                "price": data['market_data']['current_price']['usd']
+                "price": data["market_data"]["current_price"]["usd"],
             }
         }
     except Exception as e:
         print(f"Error: {e}")
         return
+
 
 async def main():
     async with httpx.AsyncClient(limits=limits, timeout=timeout) as client:
@@ -30,6 +34,7 @@ async def main():
         result = await asyncio.gather(*tasks)
 
         print(result)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
