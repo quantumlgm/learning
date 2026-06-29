0e31db076d936494fe60e2d2bb6337a507433d94
Руслан <v209861@gmail.com>
2026-06-29 16:43:35 +0300

    fix(05_advanced_gather.py): resolve bugs in async gather implementation

--- technologies/Pyhton/httpx/05_advanced_gather.py
+++ technologies/Pyhton/httpx/05_advanced_gather.py
@@ -2,25 +2,29 @@ from rich import print
 import httpx
 import asyncio
 
-
-limit = httpx.Limits(max_connections=2)
+limits = httpx.Limits(max_connections=2)
 timeout = httpx.Timeout(5)
 
 async def fetch(client: httpx.AsyncClient, coin: str):
     try:
-        params = {"ids": coin, "vs_currencies": "usd"}
+        params = {"ids": coin, "vs_currencies": "usd", "localization": False}
 
-        response = await client.get("https://api.coingecko.com/api/v3/simple/price", params=params)
+        response = await client.get(f"https://api.coingecko.com/api/v3/coins/{coin}", params=params)
         response.raise_for_status()
         
-        data = response.json()
-        return f"Coin: {data}"
+        data = response.json()            
+        return {
+            "Coin": {
+                "id": data["id"],
+                "price": data['market_data']['current_price']['usd']
+            }
+        }
     except Exception as e:
         print(f"Error: {e}")
         return
 
 async def main():
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(limits=limits, timeout=timeout) as client:
         coins = ["bitcoin", "ethereum", "solana", "ripple"]
         tasks = [fetch(client, coin) for coin in coins]
         result = await asyncio.gather(*tasks)
@@ -29,3 +33,11 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    """
+    [
+        {'Coin': {'id': 'bitcoin', 'price': 59665}},
+        {'Coin': {'id': 'ethereum', 'price': 1571.32}},
+        {'Coin': {'id': 'solana', 'price': 73.45}},
+        {'Coin': {'id': 'ripple', 'price': 1.052}}
+    ]
+    """
