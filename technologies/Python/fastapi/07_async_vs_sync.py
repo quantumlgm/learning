0855fac2c0f4b75c818e2bf91cfaf0c78371e01c
Руслан <v209861@gmail.com>
2026-07-01 18:47:41 +0300

    style(07_async_vs_sync.py): apply ruff formatting

--- technologies/Python/fastapi/07_async_vs_sync.py
+++ technologies/Python/fastapi/07_async_vs_sync.py
@@ -5,15 +5,18 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/sync")
 def sync_run():
     time.sleep(5)
     return {"satus": "Sync report done"}
 
-@app.get('/async')
+
+@app.get("/async")
 async def async_run():
     await asyncio.sleep(5)
     return {"status": "Async report is done"}
+
 
 if __name__ == "__main__":
     uvicorn.run("07_async_vs_sync:app", reload=True)
