4d685a322e2d7c770698e67d1f6288dd93ee88a2
Руслан <v209861@gmail.com>
2026-07-01 18:47:29 +0300

    feat(07_async_vs_sync.py): implement exercise

---
+++ technologies/Python/fastapi/07_async_vs_sync.py
@@ -0,0 +1,19 @@
+import asyncio
+import time
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/sync")
+def sync_run():
+    time.sleep(5)
+    return {"satus": "Sync report done"}
+
+@app.get('/async')
+async def async_run():
+    await asyncio.sleep(5)
+    return {"status": "Async report is done"}
+
+if __name__ == "__main__":
+    uvicorn.run("07_async_vs_sync:app", reload=True)
