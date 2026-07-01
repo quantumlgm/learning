23c98387a69237d891bdcf4fce1ea76e14584b56
Руслан <v209861@gmail.com>
2026-07-01 18:05:45 +0300

    style(06_status_codes_exceptions.py): apply ruff formatting

--- technologies/Python/fastapi/06_status_codes_exceptions.py
+++ technologies/Python/fastapi/06_status_codes_exceptions.py
@@ -6,6 +6,7 @@ from fake_db import employees_db
 
 app = FastAPI()
 
+
 class EmployeeSchemaIn(BaseModel):
     salary: float
     internal_token: str
@@ -14,18 +15,20 @@ class EmployeeSchemaIn(BaseModel):
     department: str
     skills: list[str] = []
 
+
 class EmployeeSchemaOut(BaseModel):
     first_name: str
     last_name: str
     department: str
     skills: list[str] = []
 
+
 @app.post("/employees", status_code=201)
 def create_employe(data: EmployeeSchemaIn):
     new_data = data.model_dump()
     new_data["employee_id"] = len(employees_db) + 1
     employees_db.append(data)
-    
+
 
 @app.get("/employees/{employee_id}", response_model=EmployeeSchemaOut)
 def get_employee(employee_id: int):
@@ -33,6 +36,7 @@ def get_employee(employee_id: int):
         if employee["employee_id"] == employee_id:
             return employee
     raise HTTPException(status_code=404, detail="User not found")
+
 
 if __name__ == "__main__":
     uvicorn.run("06_status_codes_exceptions:app", reload=True)
