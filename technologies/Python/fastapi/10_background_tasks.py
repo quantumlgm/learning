20e3ed67cdeda84c0b22f6637897c93fd527ffcf
Руслан <v209861@gmail.com>
2026-07-02 16:39:14 +0300

    style(10_background_tasks.py): apply ruff formatting

--- technologies/Python/fastapi/10_background_tasks.py
+++ technologies/Python/fastapi/10_background_tasks.py
@@ -1,5 +1,3 @@
-
-
 import asyncio
 from rich import print
 import uvicorn
@@ -11,12 +9,11 @@ app = FastAPI()
 
 employees_db = employees_db.copy()
 
+
 async def generate_report(id: int, employee: dict[str, str | int]):
     await asyncio.sleep(3)
-    print({
-        "id": id,
-        "employee": employee
-    })
+    print({"id": id, "employee": employee})
+
 
 @app.delete("/employees/{employee_id}")
 async def del_employee(employee_id: int, bg_tasks: BackgroundTasks):
@@ -24,8 +21,12 @@ async def del_employee(employee_id: int, bg_tasks: BackgroundTasks):
         if employee["employee_id"] == employee_id:
             employees_db.remove(employee)
             bg_tasks.add_task(generate_report, employee_id, employee)
-            return {"status": 200, "detail": "Your report has sent to your email. Please wait",}
+            return {
+                "status": 200,
+                "detail": "Your report has sent to your email. Please wait",
+            }
     raise HTTPException(status_code=404, detail="User doesn't founded")
-        
+
+
 if __name__ == "__main__":
     uvicorn.run("10_background_tasks:app", reload=True)
