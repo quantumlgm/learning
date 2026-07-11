b9d7e531b3a01e5985830f2865f9447f09592758
Руслан <v209861@gmail.com>
2026-07-11 13:01:29 +0300

    feat(02_core_tables_and_insert.py): implement table creation and data insertion

--- technologies/Python/sqlalchemy/02_core_tables_and_insert.py
+++ technologies/Python/sqlalchemy/02_core_tables_and_insert.py
@@ -1,13 +1,25 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine
+from sqlalchemy import (
+    Table,
+    Column,
+    Integer,
+    String,
+    MetaData,
+    create_engine,
+    text,
+    insert,
+)
 from config import settings
+
 
 engine = create_engine(
     url=settings.DB_URL_psycopg, pool_size=5, echo=True, max_overflow=10
 )
 
+
 metadata_obj = MetaData()
 
-employees_table = Table(
+
+workers_table = Table(
     "workers",
     metadata_obj,
     Column("id", Integer, primary_key=True),
@@ -17,7 +29,35 @@ employees_table = Table(
 
 
 def create_tables():
+    # 'drop_all' delete all that we add to metadata object
+    metadata_obj.drop_all(engine)
     metadata_obj.create_all(engine)
 
 
 create_tables()
+
+
+def insert_data_raw():
+    with engine.begin() as conn:
+        stmt = """
+        insert into workers (username, age)
+        values ('Ruslan Quantum', 17)
+        """
+        conn.execute(text(stmt))
+
+
+insert_data_raw()
+
+
+def insert_data_new():
+    with engine.begin() as conn:
+        stmt = insert(workers_table).values(
+            [
+                {"username": "Ruslan Future", "age": 1700},
+                {"username": "Linus Torwalds", "age": 56},
+            ]
+        )
+        conn.execute(stmt)
+
+
+insert_data_new()
--- technologies/Python/sqlalchemy/config.py
+++ technologies/Python/sqlalchemy/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
+
 class Settings(BaseSettings):
     DB_NAME: str
     DB_HOST: str
@@ -7,12 +8,15 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_USER: str
 
+    @property
     def DB_URL_psycopg(self):
-        return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}//{self.DB_NAME}'
-    
-    def DB_URL_psycopg(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}//{self.DB_NAME}'
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file='.env')
+    @property
+    def DB_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
