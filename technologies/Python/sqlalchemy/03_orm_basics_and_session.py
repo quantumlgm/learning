bfe873cbf44525296ee04e56dc752e6282bf9c44
Руслан <v209861@gmail.com>
2026-07-12 13:37:31 +0300

    style(sqlalchemy): apply ruff formatting

--- technologies/Python/sqlalchemy/03_orm_basics_and_session.py
+++ technologies/Python/sqlalchemy/03_orm_basics_and_session.py
@@ -27,24 +27,26 @@ with sessionf() as session:
     pass
 # OR
 asessionf = async_sessionmaker(aengine, expire_on_commit=False)
+
+
 async def async_session():
     async with asessionf() as asession:
         pass
 
+
 class Base(DeclarativeBase):
     pass
 
+
 class WorkersOrm(Base):
-    __tablename__ = 'workers_orm'
+    __tablename__ = "workers_orm"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
     age: Mapped[int]
 
+
 def insert_data():
-    worker = WorkersOrm(
-        username='Ruslan Orm',
-        age=17
-    )
+    worker = WorkersOrm(username="Ruslan Orm", age=17)
     with sessionf() as session:
         session.add(worker)
         session.commit()
