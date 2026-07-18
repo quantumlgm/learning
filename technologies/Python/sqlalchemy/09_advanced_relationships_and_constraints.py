9510de7c06bdf13a2f7cef124292be524b6530e3
Руслан <v209861@gmail.com>
2026-07-18 23:33:50 +0300

    feat(09_advanced_relationships_and_constraints.py): implement exercise

---
+++ technologies/Python/sqlalchemy/09_advanced_relationships_and_constraints.py
@@ -0,0 +1,100 @@
+import select
+from rich import print
+from sqlalchemy import CheckConstraint, ForeignKey, Index, String, create_engine, desc, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, contains_eager, mapped_column, relationship, selectinload, sessionmaker
+from config import settings
+
+
+engine = create_engine(settings.DB_URL_psycopg, echo=True)
+
+
+sessionf = sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    def __repr__(self):
+        cols = []
+        for col in self.__table__.columns.keys():
+            cols.append(f"{col}={getattr(self, col)}")
+        return f"<{self.__class__.__name__} {','.join(cols)}>"
+
+
+class WorkersOrm(Base):
+    __tablename__ = "workers_orm"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255))
+    age: Mapped[int]
+    
+    resumes: Mapped[list["ResumesOrm"]] = relationship(        
+        back_populates="worker",
+        # backref="worker"            
+    )
+    """
+    backref="..." is implicit approach for make relationships. 
+    When we use a backref in one table, we don't need to use it
+    in other table
+    """
+    resumes_adult: Mapped[list["ResumesOrm"]] = relationship(
+        back_populates="worker",
+        primaryjoin="and_(WorkersOrm.id == ResumesOrm.worker_id, ResumesOrm.compensation > 50000)",
+        order_by="ResumesOrm.id.desc()"
+        # lazy="selectin"
+    )
+    """
+    If we use selectin type of uploading,
+    then we shouldn't specify this type in
+    query options. And this is bad practice, 
+    in that it isn't explictly
+    """
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes_orm"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    compensation: Mapped[int]
+    worker_id: Mapped[int] = mapped_column(ForeignKey('workers_orm.id'))
+
+    worker: Mapped["WorkersOrm"] = relationship(
+        back_populates="resumes"
+    )
+
+    __table_args__ = (
+        Index("title_idx", "title"), 
+        CheckConstraint('compensation >= 0', name='check_compensation')
+    )
+
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+
+create_tables()
+
+
+def age_select():
+    with sessionf.begin() as session:
+        stmt = (
+            select(WorkersOrm)            
+            .options(selectinload(WorkersOrm.resumes_adult))
+        )
+        res = session.execute(stmt).scalars().all()
+        print(res)
+        
+
+age_select()
+
+
+def contains_eager_select():
+    with sessionf.begin() as session:
+        stmt = (
+            select(WorkersOrm)            
+            .join(WorkersOrm.resumes)
+            .options(contains_eager(WorkersOrm.resumes))
+            .filter(WorkersOrm.age > 18)
+            .limit(3)
+        )
+        res = session.execute(stmt).unique().scalars().all()
+        print(res)
+
+
+contains_eager_select()
