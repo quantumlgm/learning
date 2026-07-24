ac5b64cf0ba3268093cee5ca29f993c1e1f6fc1c
Ruslan <v209861@gmail.com>
2026-07-24 13:48:01 +0300

    feat(deleter): move functions into class

--- technologies/Git/blind_tree/deleter.py
+++ technologies/Git/blind_tree/deleter.py
@@ -6,22 +6,28 @@ This file was created as an example of working with Git branches.
 from typing import Callable
 
 
-def delete(file: object):
-    return f"{file} - files was deleted"
+class FilePanel:
+    @staticmethod    
+    def delete(file: object):
+        return f"{file} - files was deleted"
 
 
-def create(file: object):
-    return f"{file} - file was created"
+    @staticmethod
+    def create(file: object):
+        return f"{file} - file was created"
 
 
-def generate_deleter(func: Callable[object, str]):
-    print("Process is starting")
-    yield func
-    print("Process is completed")
+    @staticmethod
+    def generate_deleter(func: Callable[object, str]):
+        print("Process is starting")
+        yield func
+        print("Process is completed")
+
 
 if __name__ == '__main__':
-    delete("passwords.txt")
-    create(["Application", "Site", "Http"])
+    fp = FilePanel()
+    fp.delete("passwords.txt")
+    fp.create(["Application", "Site", "Http"])
      
-    for s in generate_deleter(delete("main.py")):
+    for s in fp.generate_deleter(fp.delete("main.py")):
         print(s)
