aeca371c1a0e1df22b52a9ad17f1d2544b844bb8
Ruslan <v209861@gmail.com>
2026-07-24 12:10:04 +0300

    feature(blind_tree): generator.py was deleted and deleter.py was added

---
+++ technologies/Git/blind_tree/delteter.py
@@ -0,0 +1,11 @@
+"""
+This file was created as an example of working with Git branches.
+"""
+
+
+def delete(file: object):
+    return f"{file} - files was deleted"
+
+
+if __name__ == '__main__':
+    delete("passwords.txt")
