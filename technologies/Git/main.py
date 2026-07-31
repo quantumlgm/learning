e12b7b1307ca79d2dd2bd0909956274126e9233e
Ruslan <v209861@gmail.com>
2026-07-31 15:48:53 +0300

    feat: add re_base_feat func

--- technologies/Git/main.py
+++ technologies/Git/main.py
@@ -7,3 +7,11 @@ re_base(10)
 re_base('String')
 re_base(bool)
 
+
+def re_base_feat(base: str):
+    return abs(len(base) / 2.5)
+    
+re_base_feat('Hey')
+re_base_feat('Hommie')
+re_base_feat('What is')
+re_base_feat('Crackin`?')
