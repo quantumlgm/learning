8910b086f56dad175b1d2ee4d11dad7d270ce45c
Ruslan <v209861@gmail.com>
2026-08-12 22:50:09 +0300

    feat: create 03_logical_type.go

---
+++ technologies/Go/03_logical_type.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func LogicalType() {
+	// true && false
+	// true || false
+	// !true
+	// true == false
+	// true != false
+
+	var something bool
+	fmt.Println(something) // false
+}
